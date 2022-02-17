620c33914f8819f44b66e6a61333551d162940ce
Eva Houžvičková <houzvickova@icloud.com>
2022-02-17 21:04:21 +0100

    Cleaning the project from unnecessary codes

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// tady je místo pro náš program
-
-console.log('Ahoj, jsem nalinkován');
-
-let nadpis = document.querySelector('h1');
-nadpis.classList.add('zluty');
-
-let pocitadlo = 0;
-function priNajeti() {
-    console.log ('test' + pocitadlo);
-    pocitadlo++;
-}
 
 let paragraph = document.querySelector('p');
 function priNajetiZtucni() {
